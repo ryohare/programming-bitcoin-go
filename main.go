7f5d9ee72790dbbb4ba9ff91aa129b62ad2f0663
Ryan O'Hare <ryan.ohare@nydig.com>
2022-07-24 08:22:05 -0400

    finished chapter 1 :

--- main.go
+++ main.go
@@ -7,10 +7,29 @@ import (
 )
 
 func main() {
-	fe := &ecc.FieldElement{
+	fe1 := &ecc.FieldElement{
+		Num:   7,
+		Prime: 13,
+	}
+	fe2 := &ecc.FieldElement{
 		Num:   7,
 		Prime: 13,
 	}
 
-	fmt.Println(fe)
+	fea, _ := ecc.Add(fe1, fe2)
+
+	fmt.Println(fea)
+
+	feb, _ := ecc.Subtract(fe1, fe2)
+
+	fmt.Println(feb)
+
+	fem, _ := ecc.Multiply(fe1, fe2)
+
+	fmt.Println(fem)
+
+	fee, _ := ecc.Exponentiate(fe1, int64(3))
+
+	fmt.Println(fee)
+
 }
--- pkg/ecc/fieldelement.go
+++ pkg/ecc/fieldelement.go
@@ -1,13 +1,16 @@
-package finitefield
+package ecc
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type FieldElement struct {
-	Num   int
-	Prime int
+	Num   int64
+	Prime int64
 }
 
-func Make(num, prime int) *FieldElement {
+func Make(num, prime int64) *FieldElement {
 	return &FieldElement{
 		Num:   num,
 		Prime: prime,
@@ -16,6 +19,77 @@ func Make(num, prime int) *FieldElement {
 
 func Equal(fe1, fe2 FieldElement) bool {
 	return fe1.Num == fe2.Num && fe1.Prime == fe2.Prime
+}
+
+func Add(fe1, fe2 *FieldElement) (*FieldElement, error) {
+	if fe1.Prime != fe2.Prime {
+		return nil, fmt.Errorf("cannot add two numbers in different fields")
+	}
+
+	num := (fe1.Num + fe2.Num) % fe1.Prime
+
+	return &FieldElement{
+			Num:   num,
+			Prime: fe1.Prime,
+		},
+		nil
+}
+
+func Subtract(fe1, fe2 *FieldElement) (*FieldElement, error) {
+	if fe1.Prime != fe2.Prime {
+		return nil, fmt.Errorf("cannot sub two numbers in different fields")
+	}
+
+	num := (fe1.Num - fe2.Num) % fe1.Prime
+
+	return &FieldElement{
+			Num:   num,
+			Prime: fe1.Prime,
+		},
+		nil
+
+}
+
+func Multiply(fe1, fe2 *FieldElement) (*FieldElement, error) {
+	if fe1.Prime != fe2.Prime {
+		return nil, fmt.Errorf("cannot multiply two numbers in different fields")
+	}
+
+	num := (fe1.Num * fe2.Num) % fe1.Prime
+
+	return &FieldElement{
+			Num:   num,
+			Prime: fe1.Prime,
+		},
+		nil
+}
+
+func Exponentiate(fe1 *FieldElement, exponent int64) (*FieldElement, error) {
+
+	n := exponent % (fe1.Prime - 1)
+	fmt.Println(n)
+	num := int64(math.Pow(float64(fe1.Num), float64(n))) % fe1.Prime
+	fmt.Println(num)
+	return &FieldElement{
+			Num:   num,
+			Prime: fe1.Prime,
+		},
+		nil
+}
+
+func Divide(fe1, fe2 *FieldElement) (*FieldElement, error) {
+	if fe1.Prime != fe2.Prime {
+		return nil, fmt.Errorf("cannot exponentiate two numbers in different fields")
+	}
+
+	//num = self.num * pow(other.num, self.prime - 2, self.prime) % self.prime
+	num := fe1.Num * int64(math.Pow(float64(fe2.Num), float64(fe1.Prime-2))) & fe1.Prime
+
+	return &FieldElement{
+			Num:   num,
+			Prime: fe1.Prime,
+		},
+		nil
 }
 
 func (f *FieldElement) ToString() string {
