cdc583723369edab6c4e76159adc1c1edefed3d6
Ryan O'Hare <ryan.ohare@nydig.com>
2022-07-28 12:26:43 -0400

    more refactors

--- pkg/ecc/curves/secp256k1/secp256k1.go
+++ pkg/ecc/curves/secp256k1/secp256k1.go
@@ -107,3 +107,33 @@ func GetNonce() *big.Int {
 	n, _ := new(big.Int).SetString(N, 16)
 	return n
 }
+
+func VerifySignature(pk PrivateKey, z *big.Int, sig *Signature) (bool, error) {
+	// validated
+	G := GetGeneratorPoint()
+	tmpSInv := new(big.Int)
+	tmpN := new(big.Int)
+	n2 := tmpN.Sub(GetNonce(), big.NewInt(2))
+	sInv := tmpSInv.Exp(sig.S, n2, GetNonce())
+	tmpSInv = new(big.Int)
+
+	// validated
+	u := tmpSInv.Mul(z, sInv)
+	u = u.Mod(u, GetNonce())
+	tmpSInv = new(big.Int)
+
+	//verified
+	v := tmpSInv.Mul(sig.R, sInv)
+	v = v.Mod(v, GetNonce())
+
+	// verified
+	uG, _ := RMultiply(*G, *u)
+	vP, _ := RMultiply(*pk.Point, *v)
+	sum, err := point.Addition(uG.Point, vP.Point)
+
+	if err != nil {
+		return false, err
+	}
+
+	return sum.X.Num.Cmp(sig.R) == 0, nil
+}
--- pkg/ecc/curves/secp256k1/secp256k1_test.go
+++ pkg/ecc/curves/secp256k1/secp256k1_test.go
@@ -103,3 +103,34 @@ func TestSigCreate(t *testing.T) {
 		t.Error("s value does not match the expected value")
 	}
 }
+
+func TestGlobalSigVerify(t *testing.T) {
+	z, _ := new(big.Int).SetString("bc62d4b80d9e36da29c16c5d4d9f11731f36052c72401a76c23c0fb5a9b74423", 16)
+	r, _ := new(big.Int).SetString("37206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c6", 16)
+	s, _ := new(big.Int).SetString("8ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec", 16)
+	px, _ := new(big.Int).SetString("04519fac3d910ca7e7138f7013706f619fa8f033e6ec6e09370ea38cee6a7574", 16)
+	py, _ := new(big.Int).SetString("82b51eab8c27c66e26c858a079bcdf4f1ada34cec420cafc7eac1a42216fb6c4", 16)
+
+	p := MakePoint(px, py)
+
+	pk := PrivateKey{
+		Secret: "secret",
+		Point:  p,
+	}
+
+	sig := &Signature{
+		R: r,
+		S: s,
+	}
+
+	res, err := VerifySignature(pk, z, sig)
+
+	if err != nil {
+		t.Errorf("failed to verify signature because %s", err.Error())
+	}
+
+	if res != true {
+		t.Errorf("signature failed verification")
+	}
+
+}
