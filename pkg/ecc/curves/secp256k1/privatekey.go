ba907b9ce2620a129de9107fa10dcdb908f5a4cc
Ryan O'Hare <ryan.ohare@nydig.com>
2022-07-28 06:48:57 -0400

    added tests for privatekey

---
+++ pkg/ecc/curves/secp256k1/privatekey.go
@@ -0,0 +1,97 @@
+package secp256k1
+
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+
+	point "github.com/ryohare/programming-bitcoin-go/pkg/ecc/point"
+)
+
+type PrivateKey struct {
+	Secret string
+	Point  *point.Point
+}
+
+func MakePrivateKey(secret string) (*PrivateKey, error) {
+	pk := &PrivateKey{}
+	s := new(big.Int).SetBytes([]byte(secret))
+	pk.Secret = secret
+	var err error
+	pk.Point, err = RMultiply(*GetGeneratorPoint(), *s)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return pk, nil
+}
+
+func (p PrivateKey) Hex() string {
+	return fmt.Sprintf("0x%.64x", []byte(p.Secret))
+}
+
+func (pk PrivateKey) Sign(z *big.Int) (*Signature, error) {
+
+	// k - 32 bytes = 256 bit K
+	b := make([]byte, 32)
+	_, err := rand.Read(b)
+	if err != nil {
+		return nil, err
+	}
+	k := new(big.Int).SetBytes(b)
+
+	// // r
+	// kG, err := RMultiply(*GetGeneratorPoint(), *k)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// r := kG.X.Num
+
+	// // kInt
+	// n2 := new(big.Int).Sub(GetNonce(), big.NewInt(2))
+	// kInv := new(big.Int).Exp(k, n2, GetNonce())
+
+	// // s
+	// secret := new(big.Int).SetBytes([]byte(p.Secret))
+	// rs := new(big.Int).Mul(r, secret)
+	// rs = rs.Add(rs, z)
+	// rs = rs.Mul(kInv, rs)
+	// s := rs.Mod(rs, GetNonce())
+
+	// // if s > N/2:
+	// if s.Cmp(new(big.Int).Div(GetNonce(), big.NewInt(2))) > 0 {
+	// 	s = new(big.Int).Sub(GetNonce(), s)
+	// }
+
+	// return &Signature{
+	// 		R: r,
+	// 		S: s,
+	// 	},
+	// 	nil
+
+	e := new(big.Int).SetBytes([]byte(pk.Secret))
+	G := GetGeneratorPoint()
+	N := GetNonce()
+
+	rPoint, err := RMultiply(*G, *k)
+	if err != nil {
+		return nil, err
+	}
+	r := rPoint.X.Num
+
+	n2 := new(big.Int).Sub(N, big.NewInt(2))
+
+	tmp := big.NewInt(0)
+	kInv := tmp.Exp(k, n2, N)
+	s := new(big.Int).Mul(r, e)
+	s = new(big.Int).Add(s, z)
+	s = s.Mul(s, kInv)
+	s = s.Mod(s, N)
+
+	return &Signature{
+			R: r,
+			S: s,
+		},
+		nil
+}
---
+++ pkg/ecc/curves/secp256k1/privatekey_test.go
@@ -0,0 +1,55 @@
+package secp256k1
+
+import (
+	"math/big"
+	"testing"
+
+	point "github.com/ryohare/programming-bitcoin-go/pkg/ecc/point"
+)
+
+func TestHex(t *testing.T) {
+	message := "message"
+	secret := new(big.Int).SetBytes([]byte(message))
+	p, _ := point.RMultiply(GetGeneratorPoint(), *secret)
+	pk := PrivateKey{
+		Secret: "message",
+		Point:  p,
+	}
+
+	pk.Hex()
+}
+
+// def test_sign(self):
+// pk = PrivateKey(randint(0, N))
+// z = randint(0, 2**256)
+// sig = pk.sign(z)
+// self.assertTrue(pk.point.verify(z, sig))
+func TestSignMessage(t *testing.T) {
+	pk, err := MakePrivateKey("rnd.String()")
+
+	if err != nil {
+		t.Errorf("failed to make private key because %s", err.Error())
+	}
+
+	message := new(big.Int).SetBytes([]byte("secret message"))
+
+	if err != nil {
+		t.Errorf("failed to get a random z value because %s", err.Error())
+	}
+
+	sig, err := pk.Sign(message)
+
+	if err != nil {
+		t.Errorf("failed to sign message because %s", err.Error())
+	}
+
+	status, err := sig.VerifySignature(pk.Point, message, sig)
+
+	if err != nil {
+		t.Errorf("failed to verify signature because %s", err.Error())
+	}
+
+	if !status {
+		t.Error("failed to verify signature")
+	}
+}
