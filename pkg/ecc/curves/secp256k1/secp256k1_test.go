995b60f02868b0644bd9465d26b339d4b03515ea
Ryan O'Hare <ryan.ohare@nydig.com>
2022-07-31 11:04:38 -0700

    finished adding Der encoding for signatures

--- pkg/ecc/curves/secp256k1/secp256k1_test.go
+++ pkg/ecc/curves/secp256k1/secp256k1_test.go
@@ -218,3 +218,14 @@ func TestParse(t *testing.T) {
 		t.Error("compressed sec keys are not the same")
 	}
 }
+
+func TestDer(t *testing.T) {
+	r, _ := new(big.Int).SetString("37206a0610995c58074999cb9767b87af4c4978db68c06e8e6e81d282047a7c6", 16)
+	s, _ := new(big.Int).SetString("8ca63759c1157ebeaec0d03cecca119fc9a75bf8e6d0fa65c841c8e2738cdaec", 16)
+	sig := Signature{
+		R: r,
+		S: s,
+	}
+
+	sig.Der()
+}
--- pkg/ecc/curves/secp256k1/signature.go
+++ pkg/ecc/curves/secp256k1/signature.go
@@ -1,6 +1,7 @@
 package secp256k1
 
 import (
+	"fmt"
 	"math/big"
 )
 
@@ -38,3 +39,55 @@ type Signature struct {
 
 // 	return sum.X.Num.Cmp(sig.R) == 0, nil
 // }
+
+// def der(self):
+// rbin = self.r.to_bytes(32, byteorder='big') # remove all null bytes at the beginning rbin = rbin.lstrip(b'\x00')
+// # if rbin has a high bit, add a \x00
+// if rbin[0] & 0x80:
+// rbin = b'\x00' + rbin
+// result = bytes([2, len(rbin)]) + rbin
+// sbin = self.s.to_bytes(32, byteorder='big') # remove all null bytes at the beginning sbin = sbin.lstrip(b'\x00')
+// # if sbin has a high bit, add a \x00
+// if sbin[0] & 0x80:
+// sbin = b'\x00' + sbin
+// result += bytes([2, len(sbin)]) + sbin return bytes([0x30, len(result)]) + result
+func (s Signature) Der() []byte {
+	rbin := s.R.Bytes()
+	sbin := s.S.Bytes()
+
+	// strip out the leading binaries
+	// dont think this is necessary in golang since we get the bytes array on deman
+
+	// if rbin has a high bit, add a \x00
+	if rbin[0]&0x80 != 0 {
+		rbin = append([]byte{0x00}, rbin...)
+	}
+
+	var resbin []byte
+	resbin = append([]byte{0x02}, resbin...)
+	resbin = append(resbin, byte(len(rbin)))
+	resbin = append(resbin, rbin...)
+	result := resbin
+
+	// if sbin has a high bit, add a \x00
+	if sbin[0]&0x80 != 0 {
+		sbin = append([]byte{0x00}, sbin...)
+	}
+
+	var sesbin []byte
+	sesbin = append([]byte{0x02}, sesbin...)
+	sesbin = append(sesbin, byte(len(sbin)))
+	sesbin = append(sesbin, sbin...)
+	fmt.Printf("%x\n", sesbin)
+	fmt.Printf("%x\n", result)
+	// result = new(big.Int).Add(new(big.Int).SetBytes(sesbin), new(big.Int).SetBytes(result)).Bytes()
+	result = append(result, sesbin...)
+
+	fmt.Printf("%x\n", result)
+
+	result = append([]byte{0x30, byte(len(result))}, result...)
+	fmt.Printf("%x\n", result)
+	// sesbin = append([]byte{0x30}, result...)
+	// fmt.Printf("%x\n", sesbin)
+	return result
+}
