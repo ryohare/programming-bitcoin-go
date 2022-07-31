61948a40b85013c8059a126555fe9fe8796789dc
Ryan O'Hare <ryan.ohare@nydig.com>
2022-07-31 11:51:50 -0700

    added base58 encode function

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -6,18 +6,7 @@ import (
 	"math/big"
 )
 
-// def hash256(s):
-//     '''two rounds of sha256'''
-//     return hashlib.sha256(hashlib.sha256(s).digest()).digest()
-// func Hash256(s big.Int) *big.Int {
-// 	h := sha256.New()
-// 	hh := sha256.New()
-// 	h.Write(s.Bytes())
-// 	hh.Write(h.Sum(nil))
-// 	digest := hh.Sum(nil)
-
-// 	return new(big.Int).SetBytes(digest)
-// }
+const BASE58_ALPHABET = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
 
 func Hash256(s []byte) []byte {
 	h := sha256.New()
@@ -36,4 +25,40 @@ func ToHexInt(n *big.Int) string {
 
 func ToHexRat(n *big.Rat) string {
 	return fmt.Sprintf("%x", n) // or %x or upper case
+}
+
+func EncodeBase58(s []byte) []byte {
+	count := 0
+
+	for _, c := range s {
+		if c == 0 {
+			count += 1
+		} else {
+			break
+		}
+	}
+
+	num := new(big.Int).SetBytes(s)
+
+	var prefix []byte
+	var result []byte
+	for i := 0; i < count; i++ {
+		prefix = append(prefix, 0x01)
+	}
+
+	for {
+		if num.Cmp(big.NewInt(0)) <= 0 {
+			break
+		}
+		var mod *big.Int
+		num, mod = new(big.Int).DivMod(num, big.NewInt(58), big.NewInt(58))
+		b := byte(BASE58_ALPHABET[mod.Int64()])
+		result = append([]byte{b}, result...)
+	}
+
+	if prefix != nil {
+		return append(prefix, result...)
+	} else {
+		return result
+	}
 }
--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -20,3 +20,28 @@ func TestHash256(t *testing.T) {
 	}
 
 }
+
+func TestEncodeBase58(t *testing.T) {
+	// 7c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838a3d
+	// eff69ef2b1bd93a66ed5219add4fb51e11a840f404876325a1e8ffe0529a2c
+	// c7207fee197d27c618aea621406f6bf5ef6fca38681d82b2f06fddbdce6feab6
+
+	a1, _ := new(big.Int).SetString("7c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838a3d", 16)
+	b1 := EncodeBase58(a1.Bytes())
+
+	if string(b1) != "9MA8fRQrT4u8Zj8ZRd6MAiiyaxb2Y1CMpvVkHQu5hVM6" {
+		t.Error("base58 encoding failed")
+	}
+	a2, _ := new(big.Int).SetString("eff69ef2b1bd93a66ed5219add4fb51e11a840f404876325a1e8ffe0529a2c", 16)
+	b2 := EncodeBase58(a2.Bytes())
+
+	if string(b2) != "4fE3H2E6XMp4SsxtwinF7w9a34ooUrwWe4WsW1458Pd" {
+		t.Error("base58 encoding failed")
+	}
+	a3, _ := new(big.Int).SetString("c7207fee197d27c618aea621406f6bf5ef6fca38681d82b2f06fddbdce6feab6", 16)
+	b3 := EncodeBase58(a3.Bytes())
+
+	if string(b3) != "EQJsjkd6JaGwxrjEhfeqPenqHwrBmPQZjJGNSCHBkcF7" {
+		t.Error("base58 encoding failed")
+	}
+}
