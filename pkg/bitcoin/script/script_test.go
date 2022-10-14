9ce780daab4f117ec9bcbc516599b3b5f8da4f1b
Ryan O'Hare <ryan.ohare@nydig.com>
2022-10-13 20:32:15 -0400

    finished up scripting section

--- pkg/bitcoin/script/script_test.go
+++ pkg/bitcoin/script/script_test.go
@@ -109,5 +109,53 @@ func TestCombine(t *testing.T) {
 	if !combinedScript.Evaluate(new(big.Int).SetBytes(zBytes), 0, 0, 0) {
 		t.Fatalf("evaulate failed")
 	}
+}
 
+func TestScriptOpCodes(t *testing.T) {
+	// X
+	// OP_DUP
+	// OP_DUP
+	// OP_MUL
+	// OP_ADD
+	// OP_6
+	// OP_EQUAL
+	// solving the question comes down to 6 = x^2+x
+	scriptPubkeyBytes := []byte{0x76, 0x76, 0x95, 0x93, 0x56, 0x87}
+	scriptPubKey := &Script{
+		RawScript: scriptPubkeyBytes,
+	}
+	for _, b := range scriptPubkeyBytes {
+		scriptPubKey.Commands = append(scriptPubKey.Commands, Command{Bytes: []byte{b}})
+	}
+
+	// solving x^2+x, x=2 >>>> op_2
+	scriptSigBytes := []byte{0x52}
+	scriptSig := &Script{
+		RawScript: scriptPubkeyBytes,
+	}
+	scriptSig.Commands = append(scriptSig.Commands, Command{Bytes: scriptSigBytes})
+	combinedScript := Combine(*scriptPubKey, *scriptSig)
+
+	if !combinedScript.Evaluate(big.NewInt(0), 0, 0, 0) {
+		t.Fatalf("failed to evaulate the script")
+	}
+}
+
+func TestCollision(t *testing.T) {
+	scriptPubkeyBytes := []byte{0x6e, 0x87, 0x91, 0x69, 0xa7, 0x7c, 0xa7, 0x87}
+	c1Bytes, _ := hex.DecodeString("55044462d312e330a25e2e3cfd30a0a0a312030206f626a0a3c3c2f57696474682032203020522f4865696768742033203020522f547970652034203020522f537562747970652035203020522f46696c7465722036203020522f436f6c6f7253706163652037203020522f4c656e6774682038203020522f42697473506572436f6d706f6e656e7420383e3e0a73747265616d0affd8fffe00245348412d3120697320646561642121212121852fec092339759c39b1a1c63c4c97e1fffe017f46dc93a6b67e013b029aaa1db2560b45ca67d688c7f84b8c4c791fe02b3df614f86db1690901c56b45c1530afedfb76038e972722fe7ad728f0e4904e046c230570fe9d41398abe12ef5bc942be33542a4802d98b5d70f2a332ec37fac3514e74ddc0f2cc1a874cd0c78305a21566461309789606bd0bf3f98cda8044629a1")
+	c2Bytes, _ := hex.DecodeString("255044462d312e330a25e2e3cfd30a0a0a312030206f626a0a3c3c2f57696474682032203020522f4865696768742033203020522f547970652034203020522f537562747970652035203020522f46696c7465722036203020522f436f6c6f7253706163652037203020522f4c656e6774682038203020522f42697473506572436f6d706f6e656e7420383e3e0a73747265616d0affd8fffe00245348412d3120697320646561642121212121852fec092339759c39b1a1c63c4c97e1fffe017346dc9166b67e118f029ab621b2560ff9ca67cca8c7f85ba84c79030c2b3de218f86db3a90901d5df45c14f26fedfb3dc38e96ac22fe7bd728f0e45bce046d23c570feb141398bb552ef5a0a82be331fea48037b8b5d71f0e332edf93ac3500eb4ddc0decc1a864790c782c76215660dd309791d06bd0af3f98cda4bc4629b1")
+
+	scriptSig := &Script{}
+	scriptSig.Commands = append(scriptSig.Commands, Command{Bytes: c1Bytes})
+	scriptSig.Commands = append(scriptSig.Commands, Command{Bytes: c2Bytes})
+
+	scriptPubKey := &Script{}
+	scriptPubKey.Commands = append(scriptPubKey.Commands, Command{Bytes: scriptPubkeyBytes})
+
+	combinedScript := Combine(*scriptPubKey, *scriptSig)
+
+	if !combinedScript.Evaluate(big.NewInt(0), 0, 0, 0) {
+		t.Fatalf("failed to evaluate script")
+	}
 }
