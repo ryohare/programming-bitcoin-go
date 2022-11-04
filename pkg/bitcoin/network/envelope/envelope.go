3092db1c061e1f147c6b91a41a36f37d1fd8304d
Ryan O'Hare <ryan.ohare@nydig.com>
2022-11-04 08:27:25 -0400

    finished serialize and related test

--- pkg/bitcoin/network/envelope/envelope.go
+++ pkg/bitcoin/network/envelope/envelope.go
@@ -19,14 +19,14 @@ const (
 var TESTNET_NETWORK_MAGIC = [...]byte{0x0b, 0x11, 0x09, 0x07}
 var MAINNET_NETWORK_MAGIC = [...]byte{0xf9, 0xbe, 0xb4, 0xd9}
 
-type Evenvelope struct {
+type Envelope struct {
 	Command []byte
 	Payload []byte
 	Magic   []byte
 }
 
-func Make(cmd, payload []byte, testnet bool) *Evenvelope {
-	env := &Evenvelope{
+func Make(cmd, payload []byte, testnet bool) *Envelope {
+	env := &Envelope{
 		Command: cmd,
 		Payload: payload,
 	}
@@ -40,7 +40,7 @@ func Make(cmd, payload []byte, testnet bool) *Evenvelope {
 	return env
 }
 
-func Parse(reader *bytes.Reader, testnet bool) (*Evenvelope, error) {
+func Parse(reader *bytes.Reader, testnet bool) (*Envelope, error) {
 	// Read the first 4 bytes of the stream as the stream magic
 	magicBytes, err := ioutil.ReadAll(io.LimitReader(reader, 4))
 	if err != nil {
@@ -95,9 +95,44 @@ func Parse(reader *bytes.Reader, testnet bool) (*Evenvelope, error) {
 		return nil, fmt.Errorf("checksomes do not match, %x vs %x", checksum, calculatedChecksumBytes)
 	}
 
-	return &Evenvelope{
+	return &Envelope{
 		Command: commandBytes,
 		Payload: payloadBytes,
 		Magic:   magicBytes,
 	}, nil
+}
+
+func (e *Envelope) Size() int {
+	return len(e.Magic) + len(e.Payload) + 12
+}
+
+func (e *Envelope) Serialize() []byte {
+
+	// first serialize the result
+	result := e.Magic
+
+	// need to backfill/padd a command to be exactly 12 bytes
+	// fresh call to make should give us 0x00'd out memory
+
+	// next push in the command
+	result = append(result, e.Command...)
+
+	// check the length of the command and add 0x00's to the
+	// byte array until the command section is exactly 12 bytes
+	for i := len(e.Command); i < 12; i++ {
+		result = append(result, 0x00)
+	}
+
+	// next push in the size of the payload
+	receiver := make([]byte, 4)
+	binary.LittleEndian.PutUint32(receiver, uint32(len(e.Payload)))
+	result = append(result, receiver...)
+
+	// add in the checksum of the payload next
+	result = append(result, utils.Hash256(e.Payload)[:4]...)
+
+	// next we push in the payload itself
+	result = append(result, e.Payload...)
+
+	return result
 }
--- pkg/bitcoin/network/envelope/envelope_test.go
+++ pkg/bitcoin/network/envelope/envelope_test.go
@@ -5,6 +5,8 @@ import (
 	"encoding/hex"
 	"fmt"
 	"testing"
+
+	"github.com/ryohare/programming-bitcoin-go/pkg/utils"
 )
 
 func TestSmoke(t *testing.T) {
@@ -21,4 +23,23 @@ func TestParse(t *testing.T) {
 	}
 
 	fmt.Printf("%v\n", env)
+}
+
+func TestSerialize(t *testing.T) {
+	msg, _ := hex.DecodeString("f9beb4d976657261636b000000000000000000005df6e0e2")
+
+	env, err := Parse(bytes.NewReader(msg), false)
+
+	if err != nil {
+		t.Fatalf("failed to parse the envelope %s", err.Error())
+	}
+
+	msgBytes := env.Serialize()
+
+	fmt.Printf("%x\n", msg)
+	fmt.Printf("%x\n", msgBytes)
+
+	if !utils.CompareByteArrays(msg, msgBytes) {
+		t.Fatal("failed to verify the bytes")
+	}
 }
